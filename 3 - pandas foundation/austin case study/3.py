d42f475cff961a97aed8077c52752183ce7e8aa6
daulet.urazalinov <daulet.urazalinov@uptake.com>
2018-08-06 20:19:08 -0400

    Update with pandas foundation samples

---
+++ 3 - pandas foundation/austin case study/3.py
@@ -0,0 +1,10 @@
+'''
+Cleaning and tidying datetime data
+
+In order to use the full power of pandas time series, you must construct a DatetimeIndex. To do so, it is necessary to clean and transform the date and time columns.
+
+The DataFrame df_dropped you created in the last exercise is provided for you and pandas has been imported as pd.
+
+Your job is to clean up the date and Time columns and combine them into a datetime collection to be used as the Index.
+'''
+
